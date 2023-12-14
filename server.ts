ffc47c4bea863210564891fbb1ff8c0b7579035e
Ignaciofabian93 <98052832+Ignaciofabian93@users.noreply.github.com>
2023-12-13 22:30:01 -0300

    changes

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import { Server, Socket } from "socket.io";
 import { config } from "dotenv";
 import user from "./routes/user";
 import room from "./routes/room";
+import messages from "./routes/messages";
 import connectDB from "./config/connectDb";
 
 config();
@@ -23,6 +24,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use("/api", user);
 app.use("/api", room);
+app.use("/api", messages);
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -37,18 +39,23 @@ const io = new Server(server, {
 interface User {
   id: string;
   username: string;
+  room: string;
 }
 
 interface Rooms {
   [roomName: string]: User[];
 }
 
+interface RoomCounts {
+  [roomName: string]: number;
+}
+
 const rooms: Rooms = {}; // Se crea el objeto de rooms
+const roomCounts: RoomCounts = {}; // Se crea el objeto de roomCounts
 io.on("connection", (socket: Socket) => {
   console.log("Socket conectado");
 
   socket.on("joinRoom", (roomName: string, username: string) => {
-    console.log("socket data: ", roomName, username);
     // Crea el room si no existe
     if (!rooms[roomName]) {
       rooms[roomName] = [];
@@ -56,25 +63,33 @@ io.on("connection", (socket: Socket) => {
 
     // Unirse al room
     socket.join(roomName);
-    rooms[roomName].push({ id: socket.id, username });
+    rooms[roomName].push({ id: socket.id, username, room: roomName });
 
     // Notica a los demás usuarios del usuario que acaba de entrar
     io.to(roomName).emit("userJoined", { username, users: rooms[roomName] });
 
     // Envía mensaje de bienvenida al usuario ingresado
     socket.emit("message", {
-      username: "Server",
+      username: "Servidor",
       text: `Bienvenido al chat ${roomName}!`,
     });
+
+    roomCounts[roomName] = rooms[roomName].length;
+
+    // Notifica a los usuarios del room los usuarios conectados
+    io.to(roomName).emit("activeUsers", { room: roomName, count: roomCounts });
   });
 
   // Controla el envío de mensajes
-  socket.on("sendMessage", (roomName: string, message: string) => {
-    io.to(roomName).emit("message", {
-      username: socket.id,
-      text: message,
-    });
-  });
+  socket.on(
+    "sendMessage",
+    (roomName: string, message: string, username: string) => {
+      io.to(roomName).emit("message", {
+        username,
+        text: message,
+      });
+    }
+  );
 
   // Controla la desconexión
   socket.on("disconnect", () => {
@@ -86,6 +101,14 @@ io.on("connection", (socket: Socket) => {
       io.to(roomName).emit("userLeft", {
         id: socket.id,
         users: rooms[roomName],
+      });
+
+      roomCounts[roomName] = rooms[roomName].length;
+
+      // Notifica a los usuarios del room los usuarios que quedan conectados.
+      io.to(roomName).emit("activeUsers", {
+        room: roomName,
+        count: roomCounts,
       });
     });
   });
