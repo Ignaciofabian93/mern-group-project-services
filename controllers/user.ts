6f47d458e7ba34de69b5b4c68353e55c0c8bb064
Ignaciofabian93 <98052832+Ignaciofabian93@users.noreply.github.com>
2023-12-14 19:44:46 -0300

    service

--- controllers/user.ts
+++ controllers/user.ts
@@ -54,7 +54,9 @@ export const registerUser = async (req: Request, res: Response) => {
           .status(401)
           .json({ message: "An error ocurred while creating user" });
       } else {
-        res.status(201).json({ message: "User created successfully" });
+        res
+          .status(201)
+          .json({ message: "User created successfully", status: "ok" });
       }
     }
   } catch (error) {
